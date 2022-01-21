79edcc4e634db311d6671dcaff5ba418fb1c3d7d
Rodrigo da Silva Soares <12267645688@estudante.ufjf.br>
2022-01-21 19:01:32 -0300

    Adiciona Sprite com construtor com objeto de parâm

--- js/Main.js
+++ js/Main.js
@@ -1,6 +1,14 @@
 import Cena from "./Cena.js";
+import Sprite from "./Sprites.js";
 console.log("Hello, World");
 const canvas = document.querySelector("canvas");
-console.log(canvas);
+const ctx = canvas.getContext("2d");
+//console.log(canvas); 
 const cena1 = new Cena(canvas);
 cena1.desenhar();
+
+const pc = new Sprite({});
+const en1 = new Sprite({x:140, y:100, w:30, h:30, color:"red"});
+
+pc.desenhar(ctx);
+en1.desenhar(ctx);

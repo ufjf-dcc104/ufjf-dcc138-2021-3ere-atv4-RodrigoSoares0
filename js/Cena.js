5937c0d3b38357bc3ab428aed32d122aac2b841e
Rodrigo da Silva Soares <12267645688@estudante.ufjf.br>
2022-01-21 19:24:51 -0300

    Adiciona lista de sprites na cena

--- js/Cena.js
+++ js/Cena.js
@@ -4,11 +4,22 @@ export default class Cena
     {
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
+        this.sprites = [];
 
     }
     desenhar()
     {
         this.ctx.fillStyle = "gray";
         this.ctx.fillRect(0,0, this.canvas.width, this.canvas.height);
+
+        for(let s = 0; s<this.sprites.length; s++)
+        {
+            const sprite = this.sprites[s];
+            sprite.desenhar(this.ctx);
+        }
+    }
+    adicionar(sprite)
+    {
+        this.sprites.push(sprite);
     }
 }

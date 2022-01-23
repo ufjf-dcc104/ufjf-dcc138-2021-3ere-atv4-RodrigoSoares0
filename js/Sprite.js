24e06ee97e43c515c2ac1f7a7ade7a37a302a63b
Rodrigo da Silva Soares <12267645688@estudante.ufjf.br>
2022-01-23 15:18:28 -0300

    Refatora restrições para caixa perfeita

--- js/Sprite.js
+++ js/Sprite.js
@@ -51,20 +51,30 @@ export default class Sprite
 
     aplicaRestricoes(dt)
     {
-        this.aplicaRestricoesDireita(dt);
-        this.aplicaRestricoesEsquerda(dt);
-        this.aplicaRestricoesBaixo(dt);
-        this.aplicaRestricoesCima(dt);
+        this.aplicaRestricoesDireita(this.mx + 1, this.my-1);
+        this.aplicaRestricoesDireita(this.mx + 1, this.my);
+        this.aplicaRestricoesDireita(this.mx + 1, this.my+1);
+
+        this.aplicaRestricoesEsquerda(this.mx - 1, this.my-1);
+        this.aplicaRestricoesEsquerda(this.mx - 1, this.my);
+        this.aplicaRestricoesEsquerda(this.mx - 1, this.my+1);
+        
+        this.aplicaRestricoesBaixo(this.mx-1, this.my + 1);
+        this.aplicaRestricoesBaixo(this.mx, this.my + 1);
+        this.aplicaRestricoesBaixo(this.mx+1, this.my + 1);
+        
+        this.aplicaRestricoesCima(this.mx-1, this.my - 1);
+        this.aplicaRestricoesCima(this.mx, this.my - 1);
+        this.aplicaRestricoesCima(this.mx+1, this.my - 1);
     }
 
-    aplicaRestricoesDireita(dt)
+    aplicaRestricoesDireita(pmx, pmy)
     {
         if(this.vx > 0)
         {
             const SIZE = this.cena.mapa.SIZE;
 
-            const pmx = this.mx + 1;
-            const pmy = this.my;
+
 
             if(this.cena.mapa.tiles[pmy][pmx]!=0)
             {
@@ -87,14 +97,11 @@ export default class Sprite
         }
     }
 
-    aplicaRestricoesEsquerda(dt)
+    aplicaRestricoesEsquerda(pmx, pmy)
     {
         if(this.vx < 0)
         {
             const SIZE = this.cena.mapa.SIZE;
-
-            const pmx = this.mx - 1;
-            const pmy = this.my;
 
             if(this.cena.mapa.tiles[pmy][pmx]!=0)
             {
@@ -117,14 +124,12 @@ export default class Sprite
         }
     }
 
-    aplicaRestricoesBaixo(dt)
+    aplicaRestricoesBaixo(pmx, pmy)
     {
         if(this.vy > 0)
         {
             const SIZE = this.cena.mapa.SIZE;
 
-            const pmx = this.mx;
-            const pmy = this.my + 1;
 
             if(this.cena.mapa.tiles[pmy][pmx]!=0)
             {
@@ -147,14 +152,12 @@ export default class Sprite
         }
     }
 
-    aplicaRestricoesCima(dt)
+    aplicaRestricoesCima(pmx, pmy)
     {
         if(this.vy < 0)
         {
             const SIZE = this.cena.mapa.SIZE;
 
-            const pmx = this.mx;
-            const pmy = this.my - 1;
 
             if(this.cena.mapa.tiles[pmy][pmx]!=0)
             {
